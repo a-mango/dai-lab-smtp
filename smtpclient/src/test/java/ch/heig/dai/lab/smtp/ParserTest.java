60aae39369b88cce0772f9055dc55551fde926c7
acm <a-mango@users.noreply.github.com>
2023-11-16 14:18:53 +0100

    feat: add test skeletons

---
+++ smtpclient/src/test/java/ch/heig/dai/lab/smtp/ParserTest.java
@@ -0,0 +1,18 @@
+package ch.heig.dai.lab.smtp;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Unit test for the Parser class.
+ */
+public class ParserTest {
+    /**
+     * Demo test.
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
