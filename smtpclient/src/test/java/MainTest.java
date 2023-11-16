aa3f2d12f38cf70b413e59be43dcb99facc89579
acm <a-mango@users.noreply.github.com>
2023-11-16 14:18:53 +0100

    feat: add test skeletons

---
+++ smtpclient/src/test/java/MainTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Unit test for the Main class.
+ */
+public class MainTest {
+    /**
+     * Demo test.
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
