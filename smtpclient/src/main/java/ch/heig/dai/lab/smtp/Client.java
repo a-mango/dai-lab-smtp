3c76c08b6b40de920343391c8a14a6ab98a62bcf
Balkghar <hugo.germano8@gmail.com>
2023-11-23 14:25:33 +0100

    feat: base for all the class except of the parser

---
+++ smtpclient/src/main/java/ch/heig/dai/lab/smtp/Client.java
@@ -0,0 +1,43 @@
+package ch.heig.dai.lab.smtp;
+
+/**
+ * Send emails to an SMTP server.
+ * 
+ * @author Aubry Mangold <aubry.mangold@heig-vd.ch>
+ * @author Hugo Germano <hugo.germano@heig-vd.ch>
+ */
+public class Client {
+   /**
+    * The mails to send to the server.
+    */
+   private Mail mails[];
+
+   /**
+    * Starting point of the program.
+    *
+    * @param args Arguments for the attack, 1st argument is the list of victims,
+    *             2sd argument is the message list and the last argument is a int
+    *             for the number of group on wich the e-mail prank is played.
+    */
+   public static void main(String[] args) {
+
+   }
+
+   /**
+    * Create a client.
+    *
+    * @param victimFile  File containing list of victims to attack 😈.
+    * @param messageFile Message to send to the victims.
+    * @param groupCount  Number of groups.
+    */
+   Client(String victimFile, String messageFile, int groupCount) {
+
+   }
+
+   /**
+    * Run the client.
+    */
+   public void execute() {
+   }
+
+}
