62fa531a9199f3257a9ed3929ec7180254749c27
acm <a-mango@users.noreply.github.com>
2023-12-01 00:25:13 +0100

    fix: client not catching exceptions properly

--- smtpclient/src/main/java/ch/heig/dai/lab/smtp/Client.java
+++ smtpclient/src/main/java/ch/heig/dai/lab/smtp/Client.java
@@ -1,73 +1,89 @@
 package ch.heig.dai.lab.smtp;
 
+import java.io.FileNotFoundException;
 import java.net.Socket;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 /**
  * Send emails to an SMTP server.
- * 
+ *
  * @author Aubry Mangold <aubry.mangold@heig-vd.ch>
  * @author Hugo Germano <hugo.germano@heig-vd.ch>
  */
 public class Client {
-   /**
-    * The mails to send to the server.
-    */
-   private Mail mails[];
+    /**
+     * The mails to send to the server.
+     */
+    private final Mail[] mails;
 
-   /**
-    * Starting point of the program.
-    *
-    * @param args Arguments for the attack, 1st argument is the list of victims,
-    *             2sd argument is the message list and the last argument is a int
-    *             for the number of group on wich the e-mail prank is played.
-    */
-   public static void main(String[] args) {
-      try {
-         int nbGrp = Integer.parseInt(args[2]);
-         Client client = new Client(args[0], args[1], nbGrp);
+    /**
+     * Create a client.
+     *
+     * @param victimFile  File containing the potential victims to attack.
+     * @param messageFile File containing the message to send to the victims.
+     * @param groupCount  Number of groups.
+     */
+    Client(String victimFile, String messageFile, int groupCount) {
+        Mail[] mails;
+        try {
+            var parser = new Parser(victimFile, messageFile, groupCount);
+            mails = parser.getGroups();
+        } catch (FileNotFoundException e) {
+            mails = null;
+            System.err.println("Error: " + e.getMessage());
+            System.exit(1);
+        }
+        this.mails = mails;
+    }
 
-         client.execute();
+    /**
+     * Starting point of the program.
+     *
+     * @param args 1st argument is the list of victims
+     *             2nd argument is the message list
+     *             3rd argument is the number of groups
+     */
+    public static void main(String[] args) {
+        final String usage = "Usage: java -jar smtpclient.jar <victimFile> <messageFile> <groupCount>";
+        int argLength = 3;
 
-      } catch (Exception e) {
+        if (args.length != argLength) {
+            System.err.println("Error: " + argLength + " arguments are required.");
+            System.err.println(usage);
+            System.exit(1);
+        }
 
-      }
+        // Parse the arguments.
+        String victimFile = args[0];
+        String messageFile = args[1];
+        int groupCount = 0;
+        try {
+            groupCount = Integer.parseInt(args[2]);
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+            System.exit(1);
+        }
 
-   }
+        // Create and start the client.
+        Client client = new Client(victimFile, messageFile, groupCount);
+        client.execute();
+    }
 
-   /**
-    * Create a client.
-    *
-    * @param victimFile  File containing list of victims to attack 😈.
-    * @param messageFile Message to send to the victims.
-    * @param groupCount  Number of groups.
-    */
-   Client(String victimFile, String messageFile, int groupCount) {
+    /**
+     * Execute the client. Create a new thread for each mail and send it to the server.
+     */
+    public void execute() {
+        final int SERVER_SOCKET = 8025;
+        final String SERVER_ADDRESS = "localhost";
 
-      Parser parser = new Parser(victimFile, messageFile, groupCount);
-      mails = parser.getGroups();
-   }
-
-   /**
-    * Run the client.
-    */
-   public void execute() {
-      final int SERVER_SOCKET = 8025;
-      final String SERVER_ADRESS = "localhost";
-
-      try (var socket = new Socket(SERVER_ADRESS, SERVER_SOCKET);
-            ExecutorService executor = Executors.newVirtualThreadPerTaskExecutor()) {
-
-         for (Mail mail : mails) {
-            var handler = new Handler(socket, new Worker(mail));
-            executor.execute(handler);
-         }
-
-      } catch (Exception e) {
-         System.out.println("Error : " + e);
-      }
-
-   }
+        try (var socket = new Socket(SERVER_ADDRESS, SERVER_SOCKET);
+             ExecutorService executor = Executors.newVirtualThreadPerTaskExecutor()) {
+            for (Mail mail : mails)
+                executor.execute(new Handler(socket, new Worker(mail))); // Create a new thread for each mail.
+        } catch (Exception e) {
+            System.out.println("Error : " + e);
+        }
+    }
 
 }
